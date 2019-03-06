42735318718d85713c893d8b0a2fddf460ffcd17
Franck Chauvel <franck.chauvel@gmail.com>
2019-03-06 09:45:35 +0100

    Bumping patch version

--- storage/__init__.py
+++ storage/__init__.py
@@ -1,13 +1,11 @@
-
 __program__ = "SensApp::Storage"
 
 __copyright__ = "Copyright (C) 2018 SINTEF"
 
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 
 __license__ = "MIT"
 
 __maintainer__ = "Franck Chauvel"
 
 __email__ = "franck.chauvel@sintef.no"
-
